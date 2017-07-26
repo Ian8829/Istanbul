1d5954e7f68a8cb0c18e9e1b03d0c9fd2806f91b
taeshik Hwang <ghjkghj@naver.com>
2017-07-26 10:57:19 +0900

    Improve the Code Coverage

--- test/test_calculator_multiplication.js
+++ test/test_calculator_multiplication.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const { assert } = require("chai");
+const { multiply } = require('../');
 
 describe("multiply()", () => {
-  it("should multiply positive numbers together");
+  it("should multiply positive numbers together", () => {
+    assert.equal(multiply(1, 3), 3);
+    assert.equal(multiply(5, 100), 500);
+  });
 
   it("should multiply positive and negative numbers together");
 
